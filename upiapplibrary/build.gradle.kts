0836ec314e178c6a3c26ce792bd5f208cca3e8d3
Ganesh Sangireddi <20bq1a05l3@vvit.net>
2025-04-09 13:51:09 +0530

    some changes in build gradle

--- upiapplibrary/build.gradle.kts
+++ upiapplibrary/build.gradle.kts
@@ -17,6 +17,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
