459007d86d7a38a4bb58438c76b5c7b934fb1b8b
Ganesh Sangireddi <20bq1a05l3@vvit.net>
2025-04-09 11:50:16 +0530

    some changes in build gradle

--- upiapplibrary/build.gradle.kts
+++ upiapplibrary/build.gradle.kts
@@ -61,7 +61,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Ganesh4326"
                 artifactId = "upiapplibrary"
-                version = "v1.0.0"
+                version = "v2.0.0"
             }
         }
     }
